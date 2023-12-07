a34e9d75df14c1a6ad869033043af7e2d8f74bf5
UltraDev <petzmagnus@gmail.com>
2023-12-07 17:36:28 +0100

    Fix uninitialised parent throwing exception when adding children to a ContainerComponent in constructor

--- src/main/kotlin/net/ultragrav/fluid/component/Component.kt
+++ src/main/kotlin/net/ultragrav/fluid/component/Component.kt
@@ -13,6 +13,9 @@ abstract class Component(val dimensions: Dimensions) {
     abstract fun render(): Solid
     abstract fun click(x: Int, y: Int, clickEvent: InventoryClickEvent)
     open fun update(area: Shape = Rectangle(dimensions), solid: Solid = render()) {
+        // Not initialized yet, this update will be superseded by the one called by the parent
+        if (!::parent.isInitialized) return
+
         parent.updateChild(this, area, solid)
     }
 }
